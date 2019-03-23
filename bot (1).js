9154ad117c84f20740ddc1c699d016079a856685
LioxChannel <45736685+LioxChannel@users.noreply.github.com>
2019-03-23 14:26:44 +0100

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -4,14 +4,13 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("551042104953143306")
+let channel =     client.channels.get("558998994492194846")
 setInterval(function() {
-channel.send(`**Moataz spam , Moataz spam , Moataz spam , Moataz spam Moataz spamMoataz spamMoataz spamMoataz spamMoataz spamMoataz spamMoataz spamMoataz spamMoataz spamMoataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moatazspam , Moataz, Moataz,Moataz** ,`);
+channel.send(`**Liox spam , Liox spam , Liox spam , Liox spam Liox spam Liox spamLiox spamLiox spamLiox spamMoataz spamMoataz spamMoataz spamMoataz spamMoataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moataz spam , Moatazspam , Moataz, Moataz,Moataz** ,`);
 }, 30)
 })
-
 client.on('message', message => {
- if(message.author.id !== "557289550779645953" )return;
+ if(message.author.id !== "474167020099993640" )return;
 
 let command = "32say"
 
